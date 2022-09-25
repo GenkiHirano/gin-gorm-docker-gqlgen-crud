e57dd5574a94e6e9a3c028268a8d68fe33844d24
GenkiHirano <genki86web@gmail.com>
2022-09-25 16:03:23 +0900

    feat: DBの初期化の処理を実装

---
+++ infrastructure/db.go
@@ -0,0 +1,34 @@
+package infrastructure
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type DB struct {
+	DBName     string
+	Username   string
+	Password   string
+	Connection *gorm.DB
+}
+
+func NewDB() *DB {
+	c := NewConfig()
+	return newDB(&DB{
+		DBName:   c.DBName,
+		Username: c.Username,
+		Password: c.Password,
+	})
+}
+
+func newDB(d *DB) *DB {
+	dsn := d.Username + ":" + d.Password + "@tcp(127.0.0.1:3306)/" + d.DBName + "?charset=utf8&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn))
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	d.Connection = db
+	return d
+}
