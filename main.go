19e4c6f9ae39c6fc009d89c84dde1af6615271a0
GenkiHirano <genki86web@gmail.com>
2022-10-04 23:11:23 +0900

    feat: gormで初期データを作成

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/GenkiHirano/gin-gorm-docker-gqlgen-crud/infra"
+	"github.com/GenkiHirano/gin-gorm-docker-gqlgen-crud/model"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-}
+	db := infra.NewDB()
+	// defer db.Close()
+
+	db.AutoMigrate(&model.User{})
+	db.Create(&model.User{Name: "yamada", Age: 20})
+	db.Create(&model.User{Name: "tanaka", Age: 21})
+	db.Create(&model.User{Name: "sato", Age: 22})
+
+	// fmt.Println("Hello, World!")
+  }
