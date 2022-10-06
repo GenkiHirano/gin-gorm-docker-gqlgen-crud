80fa36932d7e24991c4686ed61743e78e1cef29b
GenkiHirano <genki86web@gmail.com>
2022-10-07 00:29:54 +0900

    feat: dbの設定を実装

---
+++ infra/db.go
@@ -0,0 +1,40 @@
+package infra
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func NewDB() *gorm.DB {
+	loadEnv()
+	dsn := dbConfig()
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("読み込みに失敗しました: %v", err)
+	}
+}
+
+func dbConfig() string {
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := "tcp(db:3306)"
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := user + ":" + pass + "@" + host + "/" + dbName
+
+	return dsn
+}
